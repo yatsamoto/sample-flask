25b4f7f64cf2474cd95fd95a802c36280454a53c
yatsamoto <diegorsilva95@gmail.com>
2024-09-15 01:56:00 -0300

    adicionado função de logout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -31,6 +31,12 @@ def login():
          print(current_user.is_authenticated)
          return jsonify({'message': 'Autenticação enviada com sucesso'}), 200
     return jsonify({'message': 'Credenciais inválidas'}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+   logout_user()
+   return jsonify({"message": "Logout realizado com sucesso!"})
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
